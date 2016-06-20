4eefb7880460136761cf99d4ac76bcf36cd25a3e
Andres Granada <oscar.andres.granada@gmail.com>
2016-06-20 16:45:23 -0500

    Update index.js
    * Added ignore extensions
    * Added use of name preprocessing

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 'use strict'
 
 let fs = require('fs')
+  , ASSETS_EXTENSIONS = 'ico,jpg,png,js,json,xml,map'.split(',')
   ;
 
 let getRoot = function getRoot(req, res) {
     let config = this.config
       , Logger = this.Logger
       ;
-    let tplName = req.params.url;
+    let tplName = req.params.url
+      , valid=true
+      ;
     if(!tplName){
         tplName = "index.html";
+    }
+    ASSETS_EXTENSIONS.forEach(function(ext){
+      if(tplName.endsWith('.'+ext)){
+        valid = false;
+      }
+    });
+    if(!valid){
+      Logger.log('warning','Requested file is an asset ['+tplName+'].');
+      return res.status(404).send("<h1>404 - Not Found</h1>Requested path not found.");
     }
     res.render(tplName, {title: "Hello"}, function renderTemplate(err, html) {
         if (err) {
